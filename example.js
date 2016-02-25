f98e43218717f0bd60cafdeb80bbfb76f44988c2
Franklin van de Meent <fr@nkl.in>
2016-02-25 03:52:04 +0100

    Example: fixed eslint errors

--- example.js
+++ example.js
@@ -1,7 +1,10 @@
 var bitminter = require ('bitminter') ();
 
 bitminter.pool.hashrate (function (err, data) {
-	if (err) { return console.log (err); }
+  if (err) {
+    console.log (err);
+    return;
+  }
 
-	console.log ('Bitminter hashrate: ' + data + ' Ghps');
+  console.log ('Bitminter hashrate: ' + data + ' Ghps');
 });
