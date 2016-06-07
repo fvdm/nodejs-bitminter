e60263452b04fcb2be8d41e6db93c7c1783557e2
Franklin van de Meent <fr@nkl.in>
2016-06-07 02:46:04 +0200

    test(script): Replace CodeClimate with Coveralls

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$CODECLIMATE_REPO_TOKEN" != "" ]; then
-  istanbul cover test.js --print none --report lcovonly
-  codeclimate-test-reporter < ./coverage/lcov.info
+result=0
+
+eslint *.js || result=1
+
+if [ "$TRAVIS" == "true" ]; then
+  istanbul cover test.js --print none --report lcovonly || result=1
+  [ "$result" -eq "0" ] && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js || result=1
 else
-  node test.js
+  node test.js || result=1
 fi
+
+exit $result
 
